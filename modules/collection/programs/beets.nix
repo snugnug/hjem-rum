5f5347a977eca65c350af1c8e446e5ecbd3b1977
nydragon <git@ccnlc.eu>
2025-05-04 15:29:45 +0200

    programs/beets: reorganize descriptions

--- modules/collection/programs/beets.nix
+++ modules/collection/programs/beets.nix
@@ -8,18 +8,21 @@
   inherit (lib.options) mkOption mkEnableOption mkPackageOption;
 
   yaml = pkgs.formats.yaml {};
+
   cfg = config.rum.programs.beets;
 in {
   options.rum.programs.beets = {
-    enable =
-      mkEnableOption "beets"
+    enable = mkEnableOption "beets";
+
+    package =
+      mkPackageOption pkgs "beets" {}
       // {
         extraDescription = ''
           To get plugins to work, you will need to override the beets derivation
           with the plugins you want:
 
           ```nix
-          pkgs.beets.override {
+          package = pkgs.beets.override {
               pluginOverrides = {
                   fish.enable = true;
                   convert.enable = true;
@@ -27,24 +30,26 @@ in {
           };
           ```
 
-          Additionally, you will have to tell beets about the plugins to load.
-          This can be done by passing a list containing their names to
-          `rum.programs.beets.settings.plugins`. Consult the [documentation] for more information.
+          Consult the [beets derivation][beets-derivation] for a list of available plugins.
 
-          [documentation]: https://beets.readthedocs.io/en/stable/reference/config.html#plugins
+          [beets-derivation]: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/audio/beets/builtin-plugins.nix
         '';
       };
-    package = mkPackageOption pkgs "beets" {};
+
     settings = mkOption {
       inherit (yaml) type;
       default = {};
       description = ''
-        Beets configuration that is written to `.config/beets/config.yaml`.
+        Beets configuration that is written to {file}`$HOME/.config/beets/config.yaml`.
         Refer to the beets [documentation] for available options.
 
-        Regarding plugins, please consult the description of [rum.programs.beets.enable](#option-rum-programs-beets-enable).
+        If you would like to use plugins, please consult the description of
+        [rum.programs.beets.package](#option-rum-programs-beets-package) and the official
+        [documentation][plugin-doc] on the plugins configuration.
 
         [documentation]: https://beets.readthedocs.io/en/stable/reference/config.html
+        [plugin-doc]: https://beets.readthedocs.io/en/stable/reference/config.html#plugins
+
       '';
     };
   };
