4b45640d0c87b822a6bc69d0bfdc44ada0ebff6a
nydragon <git@ccnlc.eu>
2025-05-05 11:46:38 +0200

    programs/beets: add example to beets.settings

--- modules/collection/programs/beets.nix
+++ modules/collection/programs/beets.nix
@@ -14,27 +14,25 @@ in {
   options.rum.programs.beets = {
     enable = mkEnableOption "beets";
 
-    package =
-      mkPackageOption pkgs "beets" {}
-      // {
-        extraDescription = ''
-          To get plugins to work, you will need to override the beets derivation
-          with the plugins you want:
+    package = mkPackageOption pkgs "beets" {
+      extraDescription = ''
+        To get plugins to work, you will need to override the beets derivation
+        with the plugins you want:
 
-          ```nix
-          package = pkgs.beets.override {
-              pluginOverrides = {
-                  fish.enable = true;
-                  convert.enable = true;
-              };
-          };
-          ```
+        ```nix
+        package = pkgs.beets.override {
+            pluginOverrides = {
+                fish.enable = true;
+                convert.enable = true;
+            };
+        };
+        ```
 
-          Consult the [beets derivation][beets-derivation] for a list of available plugins.
+        Consult the [beets derivation] for a list of available plugins.
 
-          [beets-derivation]: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/audio/beets/builtin-plugins.nix
-        '';
-      };
+        [beets derivation]: https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/audio/beets/builtin-plugins.nix
+      '';
+    };
 
     settings = mkOption {
       inherit (yaml) type;
@@ -44,13 +42,29 @@ in {
         Refer to the beets [documentation] for available options.
 
         If you would like to use plugins, please consult the description of
-        [rum.programs.beets.package](#option-rum-programs-beets-package) and the official
-        [documentation][plugin-doc] on the plugins configuration.
+        [rum.programs.beets.package](#option-rum-programs-beets-package) and the
+        [official plugin documentation] on the plugins configuration.
 
         [documentation]: https://beets.readthedocs.io/en/stable/reference/config.html
-        [plugin-doc]: https://beets.readthedocs.io/en/stable/reference/config.html#plugins
-
+        [official plugin documentation]: https://beets.readthedocs.io/en/stable/reference/config.html#plugins
       '';
+      example = {
+        plugins = "fish convert chroma";
+        directory = "/mnt/music";
+        library = "/mnt/music/library.db";
+        per_disc_numbering = "yes";
+        asciify_paths = true;
+        convert = {
+          auto = "yes";
+          format = "flac";
+          never_convert_lossy_files = "yes";
+        };
+        import = {
+          incremental = "yes";
+          bell = "yes";
+          languages = "en";
+        };
+      };
     };
   };
 
