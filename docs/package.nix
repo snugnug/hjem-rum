5ad485baf364403988f539ece203745829481e0a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-07-04 17:06:32 -0700

    docs/package: cleanup

--- docs/package.nix
+++ docs/package.nix
@@ -73,7 +73,7 @@
   };
   configJSON =
     (pkgs.nixosOptionsDoc {
-      variablelistId = "hjr-options";
+      variablelistId = "hjem-rum-options";
       warningsAreErrors = true;
       options = (evaluatedModules.options.hjem.users.type.getSubOptions []).rum;
 
@@ -110,14 +110,15 @@
     .optionsJSON;
 
   hjemRumDocs =
-    pkgs.runCommandLocal "hjr-docs" {nativeBuildInputs = [ndg];}
+    pkgs.runCommandLocal "hjem-rum-docs" {nativeBuildInputs = [ndg];}
     ''
       mkdir -p $out
 
       footer=$(cat ${./footer.html})
 
       ndg --verbose html \
-        --jobs $NIX_BUILD_CORES --title "Hjem Rum" \
+        --title "Hjem Rum"  \
+        --jobs $NIX_BUILD_CORES \
         --module-options ${configJSON}/share/doc/nixos/options.json \
         --manpage-urls ${./manpage-urls.json} \
         --options-depth 2 \
