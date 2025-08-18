a3a9336b14a9d151f13737cf618d797365b20c15
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-08-18 14:27:04 -0700

    desktops/niri: fix checkPhase

--- modules/collection/desktops/niri.nix
+++ modules/collection/desktops/niri.nix
@@ -237,7 +237,7 @@ in {
         cfg.config
       ];
       checkPhase = optionalString (cfg.package != null) ''
-        ${getExe cfg.package} validate -c "$file"
+        ${getExe cfg.package} validate -c "$target"
       '';
     };
   };
