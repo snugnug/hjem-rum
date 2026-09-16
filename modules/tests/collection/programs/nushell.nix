1f2c612f96269b81b9a9903f4350f52224341bad
GetPsyched <priyanshu@getpsyched.dev>
2026-09-16 08:03:27 +0530

    programs/nushell: properly escape $env
    Warning (SyntaxWarning): "\$" is an invalid escape sequence. Such sequences will not work in the future. Did you mean "\\$"? A raw string is also an option.

--- modules/tests/collection/programs/nushell.nix
+++ modules/tests/collection/programs/nushell.nix
@@ -54,20 +54,20 @@ in {
 
       with subtest("Verify settings."):
         machine.succeed("test -f %s" % configFile)
-        var = machine.succeed("su bob -c 'nu --config %s -c \"\$env.config.show_banner\"'" % configFile)
+        var = machine.succeed("su bob -c 'nu --config %s -c \"\\$env.config.show_banner\"'" % configFile)
         assert "false" in var, "Config does not contain show_banner = false"
-        extraVar = machine.succeed("su bob -c 'nu --config %s -c \"\$env.config.buffer_editor\"'" % configFile)
+        extraVar = machine.succeed("su bob -c 'nu --config %s -c \"\\$env.config.buffer_editor\"'" % configFile)
         assert "vi" in extraVar, "Config does not contain extraConfig string"
-        tableBasic = machine.succeed("su bob -c 'nu --config %s -c \"\$env.config.table.mode\"'" % configFile)
+        tableBasic = machine.succeed("su bob -c 'nu --config %s -c \"\\$env.config.table.mode\"'" % configFile)
         assert "basic" in tableBasic, "Config does not contain table.mode = \"basic\""
-        tableModeType = machine.succeed("su bob -c 'nu --config %s -c \"\$env.config.table.mode | describe\"'" % configFile)
+        tableModeType = machine.succeed("su bob -c 'nu --config %s -c \"\\$env.config.table.mode | describe\"'" % configFile)
         assert "string" in tableModeType, "The type validated by nu was not a string"
 
       with subtest("Verify aliases."):
         machine.succeed("su bob -c 'nu --config %s -c \"ll\"'" % configFile)
 
       with subtest("Verify environmental variables"):
-        envVar = machine.succeed("su bob -c 'nu --config %s -c \"\$env.RUM_TEST\"'" % configFile)
+        envVar = machine.succeed("su bob -c 'nu --config %s -c \"\\$env.RUM_TEST\"'" % configFile)
         assert "HEY" in envVar, "Env var RUM_TEST does not contain HEY"
 
       with subtest("Verify plugins."):
